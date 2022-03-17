a62113337ecbc8f3249210177e7342064bd544a4
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-03-17 03:39:36 +0100

    Function and Nested  loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,26 @@
 
 int main(void)
 {
-	    int num1, num2, i, n;
-	    int next_num;
+    	int num1, num2, i, n;
+	int next_num;
 
-	    n = 98;
+	n = 98;
 
-	    for (i = 1; i <= n; i++)
-	    {
-		      if (i == 98)
-		      {
-			            printf("%d, ", num2);
-		      }
-		      else
-		      {
-			            printf("%d, ", num2);
-			            next_num = num1 + num2;
-			            num1 = num2;
-			            num2 = next_num;
-		      }
-	    }
-	    printf("\n");
+	for (i = 1; i <= n; i++)
+	{
+        	if (i == 98)
+		{
+			printf("%d, ", num2);
+		}
+		else
+		{
+	        	printf("%d, ", num2);
+			next_num = num1 + num2;
+			num1 = num2;
+			num2 = next_num;
+		}
+	  }
+	  printf("\n");
 
-	    return (0);
+	  return (0);
 }
