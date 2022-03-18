e4e425631501b9faf9bc5f954da537ed7f3a1a81
Ola-Balogun Taiwo <taiwotriumphant@gmail.com>
2022-03-17 18:47:30 -0700

    More Function and Nested Loop

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints nultplies of 3 or 5
+ * Description: multiples between 0 and 100
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("\nFizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("\nFizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("\nBuzz");
+		}
+		else
+			printf("\n%d", i);
+	}
+	return (0);
+}
