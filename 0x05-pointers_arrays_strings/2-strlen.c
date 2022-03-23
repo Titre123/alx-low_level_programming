bbe515db568780e294ba93999b14f5a18037f8e3
Ola-Balogun Taiwo <taiwotriumphant@gmail.com>
2022-03-22 22:03:18 -0700

    Array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: value to evaluate
+ * Return: not
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
