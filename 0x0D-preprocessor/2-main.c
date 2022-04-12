07bc928cf40777d8e6c103ac9cb7c1dab0ea686f
Ola-Balogun Taiwo <taiwotriumphant@gmail.com>
2022-04-11 20:01:39 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name of compilation file with new line
+ * Return: 0
+ */
+
+int main()
+{
+
+	printf("%s\n", __BASE_FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef DOUBLEGUARD
+#define DOUBLEGUARD
+#define AB(x) ((x) < (0) ? (x) * (-1) : (x))
+#endif
