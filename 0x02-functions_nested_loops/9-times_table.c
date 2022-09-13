27eb293eb5b9ba4c26d3ab6d79b15bb660f2f935
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-13 23:33:37 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,45 @@
  * add extra space past single digit
  */
 
-void times_table(void){
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++){
-            _putchar(i * j);
-	    _putchar(',');
-	    _putchar(' ');
-        }
-        _putchar('\n');
-    }
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if ((c / 10) == 0)
+			{
+				if (b == 0)
+				{
+					_putchar ('0');
+				}
+				if (b != 0)
+				{
+					_putchar (' ');
+					_putchar ((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
+		}
+		_putchar ('\n');
+	}
 }
