f5bc5b3e0646f9a7a4e8d5d63a5d2ef70dcc0e9b
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-14 00:17:48 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,17 +7,17 @@
 */
 int main(void)
 {
-	    int i = 1, j = 2, total = 0;
-	    int k;
+	int i = 1, j = 2, total = 0;
+	int k;
 
-	    while (j < 4000000)
-	    {
-		    if (j % 2 == 0)
-			          total += j;
-		    k = j;
-		    j += i;
-		    i = k;
-	    }
-	    printf("%d\n", total);
-	    return (0);
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			total += j;
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%d\n", total);
+	return (0);
 }
