56ef797b848a421f5239b371303659df75bc237c
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-14 00:01:26 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,27 +7,26 @@
 */
 int main(void)
 {
-int count = 2;
-long int i = 1, j = 2;
-long int k;
+	int count = 2;
+	long int i = 1, j = 2;
+	long int k;
 
-	    printf("%lu, ", i);
-      while (count <= 50)
-      {
-	    if (count == 50)
-      {
-      printf("%lu\n", j);
+	printf("%lu, ", i);
+      	while (count <= 50)
+      	{
+	    if (count == 50){
+      		printf("%lu\n", j);
 	    }
 	    else
 	    {
-	    printf("%lu, ", j);
+	    	printf("%lu, ", j);
 	    }
 
 	    k = j;
 	    j += i;
 	    i = k;
 	    count++;
-	    }
+	}
 
- return (0);
+ 	return (0);
  }
