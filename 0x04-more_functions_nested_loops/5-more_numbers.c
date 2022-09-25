dab5ae4359ab781050353e31a4795d73f8294af2
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-25 21:56:31 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,14 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar(j + '0');
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 			}
 			else
 			{
 				_putchar(j + '0');
 			}
 		}
-		_putchar('\n');
+		printf('\n');
 	}
 }
