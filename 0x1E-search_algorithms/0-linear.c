4391e2067ae74aec3c9ecfb34f050bde72eb8436
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-10-06 00:49:10 +0100

    Create 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * linear_search - searches for the first value an element occurs
+ * using the linear search algorithm
+ * @array: array to search element
+ * @size: size of the array
+ * @value: value to search
+ *
+ * Return: the index of the found value,
+ * or -1 if the value is not found
+ */
+
+int linear_search(int *array, size_t size, int value){
+  size_t i = 0;
+  
+  if (array == NULL || size == 0)
+		return (-1);
+  
+  for (i < size; i++) {
+    printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+    if (array[i] == value)
+      return (i);
+  }
+  return (-1);
+}
