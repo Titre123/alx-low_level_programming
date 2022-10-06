3b378efd0812357b0d20fda5e1ada73e81599d65
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-10-06 01:13:50 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,17 +14,20 @@
  * or -1 if the value is not found
  */
 
-int linear_search(int *array, size_t size, int value){
-  size_t i = 0;
-  
-  if (array == NULL || size == 0)
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array || size == 0)
 		return (-1);
-  
-  for (i < size; i++)
-  {
-    printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-    if (array[i] == value)
-      return (i);
-  }
-  return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
 }
