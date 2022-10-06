5b6af176c50e65aff1b61b1e8bd273de21ed8b8d
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-10-06 01:01:44 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int second_binary(int *array, int value, size_t low, size_t high)
+void print(int *array, size_t low, size_t high)
 
 #endif
